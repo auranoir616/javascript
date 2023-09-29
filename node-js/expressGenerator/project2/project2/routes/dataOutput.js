c19cc1f869ee9b63accf60a32e551ee5d88d720e
auranoir616 <fitrafilthmongers@gmail.com>
2023-09-29 21:38:39 +0700

    berhasil menampilkan beberapa data ketabel terpisah

--- node-js/expressGenerator/project2/project2/routes/dataOutput.js
+++ node-js/expressGenerator/project2/project2/routes/dataOutput.js
@@ -7,40 +7,71 @@ const {cekuser} =require('../config/auth')
 router.get('/',cekuser, function (req, res, next) {
     res.render('dataStock/dataOut',{title: "halaman output"})
 })
-let kodearray =[]
+let namabarang = []
+router.get('/find',cekuser, async function(req, res, next){
+    try{const kodebarang = await req.query.kode
+      
+        stockIn.findOne({
+            kode: kodebarang
+        })
+        .then((data)=>{
+    
+            if(!data){
+                console.log("data tidak ditemukan")
+                console.log(namabarang)
+            }else{
+                namabarang.push({
+                    nama: data.nama,
+                    harga_satuan:data.harga_satuan
+                })
+                console.log(namabarang)
+                // res.redirect('/dataOutput',{namabarang});
+                res.redirect(`/dataOutput?namabarang=${encodeURIComponent(JSON.stringify(namabarang))}`);
 
-router.get('/:find',cekuser, function(req, res, next){
-    const kodebarang = req.query.kode
-
-    kodearray.push(kodebarang)
-    let namabarang = []
-//?coba
-//!menambahkan setiap kode dalam tabel dan menngakses harganya
-    // stockIn.find({
-    //     kode:{$in: kodearray}
-    // })
-    stockIn.findOne({
-        kode: kodebarang
-    })
-    .then((data)=>{
-
-        if(!data){
-            console.log("data tidak ditemukan")
-            console.log(kodearray)
-        }else{
-            namabarang.push({
-                nama: data.nama,
-                harga_satuan:data.harga_satuan
-            })
-            console.log(kodearray)
-            res.render('dataStock/dataOut',{namabarang})
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Terjadi kesalahan dalam pencarian data.");
+          });
         }
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send("Terjadi kesalahan dalam pencarian data.");
-      });
+        catch(err){
+            console.log(err)
+        }
+    
 })
+router.get('/hasil',cekuser, function (req, res, next) {
+    res.render('tabel', { title: "halaman output", namabarang: namabarang }); // Pass namabarang sebagai data})
+})
+// Ubah rute pertama menjadi /dataOutputFind atau yang sesuai
+// router.get('/dataOutputFind', cekuser, async function(req, res, next) {
+//     try {
+//         const kodebarang = await req.query.kode;
+      
+//         stockIn.findOne({
+//             kode: kodebarang
+//         })
+//         .then((data) => {
+//             if (!data) {
+//                 console.log("data tidak ditemukan");
+//                 console.log(namabarang);
+//             } else {
+//                 namabarang.push({
+//                     nama: data.nama,
+//                     harga_satuan: data.harga_satuan
+//                 });
+//                 console.log(namabarang);
+//                 res.redirect('/dataOutput');
+//             }
+//         })
+//         .catch((err) => {
+//             console.error(err);
+//             res.status(500).send("Terjadi kesalahan dalam pencarian data.");
+//         });
+//     } catch (err) {
+//         console.log(err);
+//     }
+// });
 
 
 module.exports = router
